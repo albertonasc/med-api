1770b242ff4517927a5c3bc2b6771e38ba8ff05d
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-13 15:20:56 -0300

    Ajuste retornos API no controller

---
+++ src/main/java/com/alberto/apimed/entity/DadosDetalhamentoMedico.java
@@ -0,0 +1,10 @@
+package com.alberto.apimed.entity;
+
+import com.alberto.apimed.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco() );
+    }
+}
