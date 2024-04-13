a650c56c202996bc22dc35182697ef13ff8c920e
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-13 13:35:39 -0300

    Novo campo ativo em Medico e inclusão da rota excluir em MedicoController

--- src/main/java/com/alberto/apimed/controller/MedicoController.java
+++ src/main/java/com/alberto/apimed/controller/MedicoController.java
@@ -28,7 +28,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -38,4 +38,10 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
 }
