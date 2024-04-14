eeca19d600343cf5b60c749b94103e7c85f74855
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-14 14:29:28 -0300

    Adicionado spring security no projeto, incluido Usuario e migration

---
+++ src/main/java/com/alberto/apimed/domain/Usuario.java
@@ -0,0 +1,22 @@
+package com.alberto.apimed.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "usuarios")
+@Table(name = "Ususarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
