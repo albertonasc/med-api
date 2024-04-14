4d115e6b629007d48b592a35fa3ecd200d03df30
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-14 12:04:39 -0300

    Tratamento de erros da api

---
+++ src/main/java/com/alberto/apimed/infra/TratadorDeErros.java
@@ -0,0 +1,26 @@
+package com.alberto.apimed.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity erro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(
+                erros.stream().map(
+                        (e) -> new DadosErroValidacao(e.getField(), e.getDefaultMessage()))
+                        .toList());
+    }
+
+}
