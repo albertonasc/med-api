6376edf80c67ba53f945078e4962356ed9d3178c
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-14 22:03:49 -0300

    Autenticação do Usuario e controle do acesso(requisição de login liberada, as outras precisam estar autenticadas )

--- src/main/java/com/alberto/apimed/infra/security/SecurityFilter.java
+++ src/main/java/com/alberto/apimed/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package com.alberto.apimed.infra.security;
 
+import com.alberto.apimed.service.AutenticacaoService;
 import com.alberto.apimed.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,27 +17,34 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private TokenService tokenService;
+    private AutenticacaoService autenticacaoService;
 
-    public SecurityFilter(TokenService tokenService) {
+    public SecurityFilter(TokenService tokenService, AutenticacaoService autenticacaoService) {
         this.tokenService = tokenService;
+        this.autenticacaoService = autenticacaoService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
 
+        if(tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = autenticacaoService.loadUserByUsername(subject);
 
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if(authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
 
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
