50f6ac488a24d5f87a3d48787a3c2bea163adc81
Alberto Nascimento <beto.bdn@gmail.com>
2024-04-14 21:13:05 -0300

    Criado filtro de segurança e recuperado/validado token da requisição

---
+++ src/main/java/com/alberto/apimed/infra/security/SecurityFilter.java
@@ -0,0 +1,40 @@
+package com.alberto.apimed.infra.security;
+
+import com.alberto.apimed.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        var subject = tokenService.getSubject(tokenJWT);
+
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
